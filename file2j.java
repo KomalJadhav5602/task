7760eb49fb30af456c2acc6aaeb0ae680204ae94
Komal Jadhav <komaljadhav5602@gmail.com>
2024-04-17 19:20:17 +0530

    this is final update

---
+++ file2j.java
@@ -0,0 +1,4 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+     1714     524    1714      10384  pty0      197609 17:00:34 /usr/bin/ps
+      524     523     524       1836  pty0      197609   Apr 15 /usr/bin/bash
+      523       1     523       8212  ?         197609   Apr 15 /usr/bin/mintty
---
+++ filej1.java
@@ -0,0 +1,4 @@
+      PID    PPID    PGID     WINPID   TTY         UID    STIME COMMAND
+     1674     524    1674      18568  pty0      197609 16:58:11 /usr/bin/ps
+      524     523     524       1836  pty0      197609   Apr 15 /usr/bin/bash
+      523       1     523       8212  ?         197609   Apr 15 /usr/bin/mintty
